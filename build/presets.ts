97a5c6f485184bbd50a5f10ca1045e462dac46e5
wulin <admin@MacBook.local>
2023-10-16 17:20:22 +0800

    build: 解决项目打包失败的问题

--- build/presets.ts
+++ build/presets.ts
@@ -1,7 +1,7 @@
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 // rollup打包分析插件
-import visualizer from 'rollup-plugin-visualizer'
+import { visualizer } from 'rollup-plugin-visualizer'
 import glsl from 'vite-plugin-glsl'
 
 export default (env, isBuild) => {
