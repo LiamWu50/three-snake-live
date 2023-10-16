853c0fbb69dd90253e7bb9cc8de15bd81133f3fd
wulin <admin@MacBook.local>
2023-10-16 17:09:55 +0800

    refactor: update project template

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import './styles/index.css'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-import React, { StrictMode } from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const rootElement = document.getElementById('root') as HTMLElement
-const root = ReactDOM.createRoot(rootElement)
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 )
